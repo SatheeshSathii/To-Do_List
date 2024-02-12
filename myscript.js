bef34164349025b88fe54b805c7da396f98c53cd
Satheesh Reddy <123052918+SatheeshSathii@users.noreply.github.com>
2024-02-12 09:47:34 +0530

    Add files via upload

---
+++ myscript.js
@@ -0,0 +1,15 @@
+var button = document.querySelector("button");
+var satheesh = document.getElementById("changeMe");
+
+/*button.onclick = function changeColor(){
+    satheesh.style.background="green";
+}*/
+function changeMe(){
+    if(satheesh.style.background = "red");
+    
+}
+
+var a=10;
+var b = 20;
+var product = a*b;
+document.write(product);
